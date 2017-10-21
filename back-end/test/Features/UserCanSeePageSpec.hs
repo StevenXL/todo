b4d2febc5e76fdf6dc5207069a48283352ce695e
Steven Leiva <leiva.steven@gmail.com>
2017-10-21 11:04:43 -0400

    Wire up integration suite.
    Note that there are a couple of steps to take before the test suite will
    pass.
    
    First, we must install and run selenium-server-standalone and
    chromedriver. For now, we are only testing using Chrome. We can install
    selenium standalone via brew. We can also install chromedriver via brew.
    
    Secondly, we should have our back-end and running and serving up a
    production build of our front-end.

---
+++ back-end/test/Features/UserCanSeePageSpec.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Features.UserCanSeePageSpec where
+
+import Test.Hspec.WebDriver
+
+main :: IO ()
+main = hspec spec
+
+chromeOnly :: [Capabilities]
+chromeOnly = [chromeCaps]
+
+spec :: Spec
+spec = do
+    describe "home page" $ do
+        session "visiting home page" $ using chromeOnly $ do
+            it "home page loads" $ runWD $
+                openPage "localhost:8080"
