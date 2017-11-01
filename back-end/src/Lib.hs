a1441de10b7f0494443d7524f7b760ec5c8dad57
Steven Leiva <leiva.steven@gmail.com>
2017-11-01 08:16:59 -0400

    Add a Todo endpoint for resting auth.

--- back-end/src/Lib.hs
+++ back-end/src/Lib.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators   #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Lib
     ( startApp
     , app
@@ -15,16 +17,25 @@ import Servant
 import Database.Persist.Postgresql (Entity, SqlBackend, selectList, runSqlPool)
 import Data.Pool (Pool)
 import Models
+import GHC.Generics
+
+data Todo = Todo { description :: String} deriving (Eq, Show, Generic)
+
+instance ToJSON Todo
+
+todos :: [Todo]
+todos = [Todo "Make Coffee", Todo "Make Code"]
 
 type UserAPI = "api" :> "users" :> Get '[JSON] [Entity User]
+type TodoAPI = "api" :> "todos" :> Get '[JSON] [Todo]
 type StaticAPI = Raw
-type WebAPI = UserAPI :<|> StaticAPI
+type WebAPI = UserAPI :<|> TodoAPI :<|> StaticAPI
 
 startApp :: Pool SqlBackend -> IO ()
 startApp pool = run 8080 $ app pool
 
 app :: Pool SqlBackend -> Application
-app pool = serve api $ userHandler' :<|> serveDirectoryFileServer "../front-end/build"
+app pool = serve api $ userHandler' :<|> return todos :<|> serveDirectoryFileServer "../front-end/build" 
     where userHandler' = userHandler pool
 
 api :: Proxy WebAPI
