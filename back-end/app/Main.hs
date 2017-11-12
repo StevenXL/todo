33ebfca862213c034b3c2a5e31e512e6abab5b66
Steven Leiva <leiva.steven@gmail.com>
2017-11-12 18:23:28 -0500

    Use monad logger.

--- back-end/app/Main.hs
+++ back-end/app/Main.hs
@@ -1,21 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
-import Lib (startApp)
-import Models (migrateAll, User(..))
-import Database.Persist.Postgresql (ConnectionString, createPostgresqlPool, SqlBackend, runMigration, runSqlPool, insert)
+import Control.Monad.Logger (runStdoutLoggingT)
 import Data.Pool (Pool)
-import Control.Monad.Logger
+import Database.Persist.Postgresql
+       (ConnectionString, SqlBackend, createPostgresqlPool, insert,
+        runMigration, runSqlPool)
+import Lib (startApp)
+import Models (User(..), migrateAll)
 
 main :: IO ()
 main = do
-    pool <- (createPostgresqlPool connStr 1) :: IO (Pool SqlBackend)
+    pool <- runStdoutLoggingT $ createPostgresqlPool connStr 1
     runSqlPool (runMigration migrateAll) pool
     startApp pool
 
 connStr :: ConnectionString
-connStr = "host=localhost port=5432 user=postgres dbname=test password=test"
-
-instance MonadLogger IO where
-    monadLoggerLog _ _ _ _ = putStrLn "Soemthing got logged"
+connStr =
+    "host=localhost port=5432 user=postgres dbname=servanttest password=test"
